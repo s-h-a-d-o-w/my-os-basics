2f308f71a2d1379b6e2f5a588fbb4931b1ff59a9
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2026-03-31 21:04:01 +0200

    Debug init paths

--- init.sh
+++ init.sh
@@ -2,17 +2,18 @@
 sudo apt update
 sudo apt upgrade -y
 
+# common tools
+sudo apt install -y vim git
+
 # zsh
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i 's/^plugins=(.*)/plugins=(git direnv kubectl zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
-# Unsure if needed to make zsh default
-# chsh -s /usr/bin/zsh
 
-# aliases
-curl -fsSL https://raw.githubusercontent.com/s-h-a-d-o-w/my-os-basics/refs/heads/main/aliases.zsh -o "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/aliases.zsh"
+# my custom aliases
+curl -fsSL https://raw.githubusercontent.com/s-h-a-d-o-w/my-os-basics/refs/heads/main/aliases.zsh -o ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/aliases.zsh
 
 # nvm
 if [[ "$1" != "no-nvm" ]]; then
